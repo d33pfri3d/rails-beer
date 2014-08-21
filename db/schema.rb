e80d5ef21a91603aeb696229e18c1bddd1139874
Shaun Dunne <thedarkpoet@gail.com>
2014-08-21 22:15:21 +0100

    add tasted on migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140821211122) do
+ActiveRecord::Schema.define(version: 20140821211441) do
 
   create_table "beers", force: true do |t|
     t.string   "name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20140821211122) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "rating"
+    t.date     "tasted_on"
   end
 
 end

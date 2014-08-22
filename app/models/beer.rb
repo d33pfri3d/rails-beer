e8467976724b8f27bf47c022849c877949f08f68
Shaun Dunne <thedarkpoet@gail.com>
2014-08-22 11:19:19 +0100

    Improve search

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -2,9 +2,12 @@ class Beer < ActiveRecord::Base
 
 	scope :tasted, ->{ where.not(tasted_on: nil) }
 	scope :recent, ->{ where('tasted_on > ?', 2.days.ago) }
-	
+
 	# A scope always returns a collection, a class method does not.
-	scope :search, ->(keyword){ where(name: keyword) if keyword.present? }
+	scope :search, ->(keyword){ where('keywords LIKE ?', "%#{keyword.downcase}%") if keyword.present? }
+
+
+	before_save :set_keywords
 
 	# def self.search(keyword)
 	# 	if keyword.present?
@@ -18,5 +21,10 @@ class Beer < ActiveRecord::Base
 	# def tasted?
 	# 	tasted_on.present?
 	# end
+
+	protected
+		def set_keywords
+			self.keywords = [name, brewery, description].map(&:downcase).join(' ')
+		end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140821211441) do
+ActiveRecord::Schema.define(version: 20140822085549) do
 
   create_table "beers", force: true do |t|
     t.string   "name"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20140821211441) do
     t.datetime "updated_at"
     t.integer  "rating"
     t.date     "tasted_on"
+    t.text     "keywords"
   end
 
 end
